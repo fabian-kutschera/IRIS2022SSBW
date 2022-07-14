0b027989e5f90881aafb8fe1f347315aa7cd95a9
fabian-kutschera <fabian.kutschera@geophysik.uni-muenchen.de>
2022-07-14 09:54:54 +0000

    correct typo

--- ieb/gmt-q42redsea.sh
+++ ieb/gmt-q42redsea.sh
@@ -1,1 +1,1 @@
-gmt psxy -JX6/6l -R3/8/0.005/500 -Ba1f0.2:"Magnitude":/a1f3:"Frequency of Earthquakes":SW redsea.xy > redsea.ps
+gmt psxy -JX6/6l -R3/8/0.001/500 -Ba1f0.2:"Magnitude":/a1f3:"Frequency of Earthquakes":SW redsea.xy > redsea.ps
